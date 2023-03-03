f90bacfa709d04cdf87b277607e157b19fecd48c
zhouwentong <zhouwentong@hualala.com>
2023-03-03 19:03:46 +0800

    update

--- src/main/java/org/example/controller/GptController.java
+++ src/main/java/org/example/controller/GptController.java
@@ -3,6 +3,7 @@ package org.example.controller;
 import cn.hutool.http.HttpUtil;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,12 +12,15 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@Slf4j
 public class GptController {
 
     @GetMapping("/gpt/ask")
     @ResponseBody
     public String ask(@RequestParam("question") String question) {
+        log.info("question: {}", question);
         String result = HttpUtil.get("https://gpt.baixing.com?p=" + question + "&k=EGW5HMV5");
+        log.info("result: {}", result);
         JSONObject jsonObject = JSON.parseObject(result);
         if (jsonObject.getIntValue("code") == 0) {
             return jsonObject.getString("data");
